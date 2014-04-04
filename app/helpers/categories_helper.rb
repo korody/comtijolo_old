5aaa2be717f57bc37f3b0b0253da1bcd7ae11b93
korody <marcoskorody@hotmail.com>
2014-04-04 11:39:00 -0300

    new pics, search done

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,9 +1,5 @@
 module CategoriesHelper
   def category_photo(name)
-    case name
-    when "Ô lá em casa!"
-    when "um quarto sobre dois"
-    end
     link_to image_tag("#{name.parameterize}.jpg", class: 'img-responsive'), casal_path
   end
 end
