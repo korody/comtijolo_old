c6a2369b795a95bc415a9971e5caac69716f3398
korody <marcoskorody@hotmail.com>
2016-07-29 12:44:30 -0300

    change default from

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 # encoding:   utf-8
 class UserMailer < ActionMailer::Base
-  default from: "tijolo@comtijolo.com"
+  default from: "korody@aerogami.com.br"
 
   def password_reset(user)
     @user = user
